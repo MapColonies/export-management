076c94cc662847094b38e163749d3ca9eb8f0c51
root <shlomiko@rafael.co.il>
2023-07-09 17:20:19 +0300

    fix: removed default collate

--- src/DAL/migration/full_schema.sql
+++ src/DAL/migration/full_schema.sql
@@ -27,9 +27,9 @@ CREATE TABLE "Task"
   "artifactCRS" "epsg_enum" NOT NULL,
   "domain" "domain_enum" NOT NULL,
   "status" "operation_status_enum" NOT NULL DEFAULT 'Pending'::"operation_status_enum",
-  "description" character varying(2000) COLLATE pg_catalog."default" NOT NULL DEFAULT ''::character varying,
+  "description" character varying(2000) NOT NULL DEFAULT ''::character varying,
   "keywords" jsonb,
-  "reason" text COLLATE pg_catalog."default" NOT NULL DEFAULT ''::text,
+  "reason" text COLLATE NOT NULL DEFAULT ''::text,
   "percentage" smallint,
   "createdAt" timestamp with time zone,
   "expiredAt" timestamp with time zone,
@@ -41,9 +41,9 @@ CREATE TABLE "TaskGeometry"
 (
   "id" serial NOT NULL,
   "exportTaskId" bigint UNIQUE NOT NULL,
-  "wkt_geometry" text COLLATE pg_catalog."default",
+  "wkt_geometry" text,
   "wkb_geometry" geometry(Geometry,4326),
-  "footprint_geojson" text COLLATE pg_catalog."default" NOT NULL,
+  "footprint_geojson" text,
   "metadata" jsonb,
   CONSTRAINT "PK_task_geometry_id" PRIMARY KEY (id),
   CONSTRAINT "FK_task_geometry_export_task_id" FOREIGN KEY ("exportTaskId") REFERENCES "Task" (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -75,16 +75,16 @@ CREATE TRIGGER task_geometry_update_geometry
 CREATE TABLE "ArtifactType"
 (
   "id" serial PRIMARY KEY ,
-  "type" text COLLATE pg_catalog."default"
+  "type" text
   -- CONSTRAINT "PK_artifact_type_id" PRIMARY KEY (id)
 );
 
 CREATE TABLE "Artifact"
 (
   "id" serial PRIMARY KEY ,
-  "name" text COLLATE pg_catalog."default",
+  "name" text,
   "artifactTypeId" bigint NOT NULL,
-  "url" text COLLATE pg_catalog."default" NOT NULL,
+  "url" text,
   "size" bigint,
   -- CONSTRAINT "PK_artifact_id" PRIMARY KEY (id),
   CONSTRAINT "FK_artifact_to_artifact_type_id" FOREIGN KEY ("artifactTypeId") REFERENCES "ArtifactType" (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -104,13 +104,13 @@ CREATE TABLE "Webhook"
 (
   "id" serial PRIMARY KEY,
   "event" "webhook_event_enum" NOT NULL,
-  "url" text COLLATE pg_catalog."default" NOT NULL
+  "url" text NOT NULL
 );
 
 CREATE TABLE "WebhookToTask"
 (
   "id" serial PRIMARY KEY,
-  "webhookId" int NOT NULL,SADF
+  "webhookId" int NOT NULL,
   "exportTaskId" int NOT NULL,
   -- CONSTRAINT "PK_artifact_to_task_id" PRIMARY KEY (id),
   CONSTRAINT "FK_webhook_to_export_task_id" FOREIGN KEY ("exportTaskId") REFERENCES "Task" (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
